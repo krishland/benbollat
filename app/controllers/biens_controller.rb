040792284e4f325302a01c2d4817fcbf8620b4cd
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-03-16 19:37:24 +0100

    mise en place du setup pour mapbox, install gem, ajout de longitude lattitude au model Bien etc...

--- app/controllers/biens_controller.rb
+++ app/controllers/biens_controller.rb
@@ -1,5 +1,5 @@
 class BiensController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :index, ]
+  skip_before_action :authenticate_user!, only: [ :index, :show ]
   def index
     @biens = policy_scope(Bien)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_162555) do
+ActiveRecord::Schema.define(version: 2022_03_16_183530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2022_03_15_162555) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["user_id"], name: "index_biens_on_user_id"
   end
 
