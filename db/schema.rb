66b433a03a5d3f57f6877fec3e69f03d0bcb062f
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-03-20 10:46:14 +0100

    ajout de champs aux deux models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_183530) do
+ActiveRecord::Schema.define(version: 2022_03_20_092723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2022_03_16_183530) do
     t.datetime "updated_at", precision: 6, null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "ville"
     t.index ["user_id"], name: "index_biens_on_user_id"
   end
 
