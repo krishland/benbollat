770bc62d262fb1c149258c7bd5b3fb132fd3ce87
Arnaud Le Garrec <legarrecarnaud.92@gmail.com>
2022-03-21 16:06:55 +0100

    création de la méthode et de la route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :biens
+  resources :biens do
+    collection do
+      get :userbiens
+    end
+  end
 end
